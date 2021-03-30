0455629449509020f7cc895fc017a04a621bb65a
Sayansree <sayansreeparia@gmail.com>
2021-03-30 11:58:49 +0530

    class controller
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- controller.h
+++ controller.h
@@ -2,3 +2,22 @@
  * email : sayansreeparia@gmail.com
  * github : https://github.com/Sayansree
  */
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "HAUVC-Model/model.h"
+#include <iostream>
+#include <vector>
+
+class controller{
+    public:
+        controller();
+        ~controller();
+        void Configure();
+
+    private:
+        std::vector<model*> dof;
+        
+};
+#endif
