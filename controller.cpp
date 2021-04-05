e0f6d3567deba03a8e7a42ee0c14060247d608c0
Sayansree <sayansreeparia@gmail.com>
2021-04-05 20:19:47 +0530

    parser exception handling
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- controller.cpp
+++ controller.cpp
@@ -15,29 +15,29 @@ controller::~controller(){
 void controller::configure(){
     boost::property_tree::ptree root,degree,basefuc,vel,disp,limit;
     try{
-    boost::property_tree::read_json("file.json",root);
-    root =root.get_child("Controller");
+        boost::property_tree::read_json("file.json",root);
+        root =root.get_child("Controller");
+        for(int i = PITCH; i <= HEAVE; i++){
+            degree=root.get_child(DOF_NAME[i]);
+            basefuc=degree.get_child("BaseFunction");
+            vel=degree.get_child("Velocity");
+            disp=degree.get_child("Displacement");
+            limit=degree.get_child("ILimit");
+            dof[i]->setCoeff(basefuc.get<double>("A2"),
+                            basefuc.get<double>("A1"),
+                            basefuc.get<double>("A0"));
+            dof[i]->setILimits(limit.get<double>("Lo"),
+                            limit.get<double>("Hi"));
+            dof[i]->setVelWeights(vel.get<double>("Kp"),
+                                vel.get<double>("Ki"),
+                                vel.get<double>("Kd"));
+            dof[i]->setDispWeights(disp.get<double>("Kp"),
+                                disp.get<double>("Ki"),
+                                disp.get<double>("Kd"));
+        }
     }catch(const boost::property_tree::ptree_error msg){
      //cout<<msg.what();
      //todo add log error
-    }
-    for(int i = PITCH; i <= HEAVE; i++){
-        degree=root.get_child(DOF_NAME[i]);
-        basefuc=degree.get_child("BaseFunction");
-        vel=degree.get_child("Velocity");
-        disp=degree.get_child("Displacement");
-        limit=degree.get_child("ILimit");
-        dof[i]->setCoeff(basefuc.get<double>("A2"),
-                         basefuc.get<double>("A1"),
-                         basefuc.get<double>("A0"));
-        dof[i]->setILimits(limit.get<double>("Lo"),
-                           limit.get<double>("Hi"));
-        dof[i]->setVelWeights(vel.get<double>("Kp"),
-                              vel.get<double>("Ki"),
-                              vel.get<double>("Kd"));
-        dof[i]->setDispWeights(disp.get<double>("Kp"),
-                               disp.get<double>("Ki"),
-                               disp.get<double>("Kd"));
     }    
 }
 void controller::init(){
