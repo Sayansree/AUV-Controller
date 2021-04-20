7ec3dd6b940c41db6f28ad7dd13c8d43fa861f91
Sayansree <sayansreeparia@gmail.com>
2021-04-20 20:03:57 +0530

    thruster plugin modification
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- ThrusterPlugin/thrusterPlugin.cpp
+++ ThrusterPlugin/thrusterPlugin.cpp
@@ -8,23 +8,25 @@ ThrusterPlugin::~ThrusterPlugin(){
 }
 void ThrusterPlugin::configure(std::vector<std::vector<double>> weights){
     this->weights=weights;
+    N=weights.size();
 }
 double ThrusterPlugin::trim(double inp){
     if(inp>1)return 1;
     if(inp<-1)return -1;
 }
 double* ThrusterPlugin::trim(double inp[]){
-    double out[weights.size()];
-    for(int i=0; i<weights.size(); i++)
+    double out[N];
+    for(int i=0; i<N; i++)
         out[i]=trim(inp[i]);
     return out;
 }
 void ThrusterPlugin::write(double dof[]){
-    double throttle[weights.size()];
-    for(int i=0; i<weights.size(); i++){
+    double throttle[N];
+    for(int i=0; i<N; i++){
         throttle[i]=0;
         for(int j=0; j<weights[i].size(); j++)
             throttle[i]+=weights[i][j]*dof[j];
     }
+    double *trimmed=trim(throttle);
     
 }
